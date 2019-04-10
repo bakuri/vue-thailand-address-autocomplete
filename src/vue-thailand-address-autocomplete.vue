95c6ce0286115e3467b2dc6f92d13c3a972d9ac6
Warasak PSV.sakorn <warasak@rebatemango.com>
2019-04-11 02:50:34 +0700

    [+] add control view props for disable enable values

--- src/vue-thailand-address-autocomplete.vue
+++ src/vue-thailand-address-autocomplete.vue
@@ -2,56 +2,70 @@
   <div class="container" v-click-outside="onClickOutside">
     <div class="label" v-if="label">{{ label }}</div>
     <div class="input-container">
-      <input type="text"
-      v-model="currentValue"
-      :placeholder="placeholder"
-      :class="{
+      <input
+        type="text"
+        v-model="currentValue"
+        :placeholder="placeholder"
+        :class="{
         'input-size-small': size === 'small',
         'input-size-default': size === 'default',
         'input-size-medium': size === 'medium',
         'input-size-large': size === 'large'
       }"
-      :style="{
+        :style="{
         'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'
       }"
-      ref="input"
-      class="input"
-      @focus="hasFocus = true"
-      @blur="hasFocus = false"
-      @keydown.up="pressArrow('up')"
-      @keydown.down="pressArrow('down')"
-      @keydown.enter="pressEnter()">
-      <div v-if="resultsFromSearch.length && isOpenListContainer"
-      ref="dropdown"
-      class="list-container"
-      :style="{'top': findListContainerPosition() }">
-        <div class="list"
-        :class="{ 'list-on-focused': itemOnFocus === index }"
-        :style="{
+        ref="input"
+        class="input"
+        @focus="hasFocus = true"
+        @blur="hasFocus = false"
+        @keydown.up="pressArrow('up')"
+        @keydown.down="pressArrow('down')"
+        @keydown.enter="pressEnter()"
+      >
+      <div
+        v-if="resultsFromSearch.length && isOpenListContainer"
+        ref="dropdown"
+        class="list-container"
+        :style="{'top': findListContainerPosition() }"
+      >
+        <div
+          class="list"
+          :class="{ 'list-on-focused': itemOnFocus === index }"
+          :style="{
           'background-color': itemOnFocus === index ? currentColor : '#fff'
         }"
-        v-for="(item, index) in resultsFromSearch"
-        :key="index"
-        @mouseover="itemOnFocus = index"
-        @mouseout="itemOnFocus = -1"
-        @click="clickSelectItem(item)">
+          v-for="(item, index) in resultsFromSearch"
+          :key="index"
+          @mouseover="itemOnFocus = index"
+          @mouseout="itemOnFocus = -1"
+          @click="clickSelectItem(item)"
+        >
           <div class="box-item-top">
-            <span class="item-first" :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }">
-              {{ itemFirst(item) }}
-            </span>
+            <span
+              v-if="itemFirstShow"
+              class="item-first"
+              :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }"
+            >{{ itemFirst(item) }}</span>
             <div class="float-right">
-              <span class="item-second" :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }">
-                {{ itemSecond(item) }}
-              </span>
-              <span class="item-third" :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }">
-                {{ itemThird(item) }}
-              </span>
+              <span
+                v-if="itemSecondShow"
+                class="item-second"
+                :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }"
+              >{{ itemSecond(item) }}</span>
+              <span
+                v-if="itemThirdShow"
+                class="item-third"
+                :class="{ 'box-item-top-focused': itemOnFocus === index && currentColor !== '#f5f5f5' }"
+              >{{ itemThird(item) }}</span>
             </div>
           </div>
           <div class="box-item-bottom">
-            <span class="item-first font-weight-bold" :style="{'color': itemOnFocus === index && currentColor !== '#f5f5f5' ? '#fff' : '#000'}">
-              {{ itemFourth(item) }}
-            </span>
+            <span
+              v-if="itemFourthShow"
+              class="item-first font-weight-bold"
+              :style="{'color': itemOnFocus === index && currentColor !== '#f5f5f5' ? '#fff' : '#000'}"
+            >{{ itemFourth(item) }}</span>
           </div>
         </div>
       </div>
@@ -60,7 +74,12 @@
 </template>
 
 <script>
-import { searchAddressByDistrict, searchAddressByAmphoe, searchAddressByProvince, searchAddressByZipcode } from 'thai-address-database'
+import {
+  searchAddressByDistrict,
+  searchAddressByAmphoe,
+  searchAddressByProvince,
+  searchAddressByZipcode
+} from 'thai-address-database'
 import vClickOutside from 'v-click-outside'
 
 export default {
@@ -78,6 +97,22 @@ export default {
     placeholder: {
       type: String
     },
+    itemFirstShow: {
+      type: Boolean,
+      default: true
+    },
+    itemSecondShow: {
+      type: Boolean,
+      default: true
+    },
+    itemThirdShow: {
+      type: Boolean,
+      default: true
+    },
+    itemFourthShow: {
+      type: Boolean,
+      default: true
+    },
     color: {
       type: String
     },
@@ -86,7 +121,7 @@ export default {
       default: 'default'
     }
   },
-  data () {
+  data() {
     return {
       currentValue: this.value,
       currentColor: this.color || '#f5f5f5',
@@ -99,27 +134,28 @@ export default {
     clickOutside: vClickOutside.directive
   },
   computed: {
-    resultsFromSearch () {
+    resultsFromSearch() {
       if (this.type) {
         if (this.type === 'district') return this.resultsFromSearchByDistrict
         else if (this.type === 'amphoe') return this.resultsFromSearchByAmphoe
-        else if (this.type === 'province') return this.resultsFromSearchByProvince
+        else if (this.type === 'province')
+          return this.resultsFromSearchByProvince
         else if (this.type === 'zipcode') return this.resultsFromSearchByZipcode
       } else {
         this._errorLog('type is undefined.')
         return []
       }
     },
-    resultsFromSearchByDistrict () {
+    resultsFromSearchByDistrict() {
       return searchAddressByDistrict(this.currentValue)
     },
-    resultsFromSearchByAmphoe () {
+    resultsFromSearchByAmphoe() {
       return searchAddressByAmphoe(this.currentValue)
     },
-    resultsFromSearchByProvince () {
+    resultsFromSearchByProvince() {
       return searchAddressByProvince(this.currentValue)
     },
-    resultsFromSearchByZipcode () {
+    resultsFromSearchByZipcode() {
       return searchAddressByZipcode(this.currentValue)
     }
   },
@@ -129,7 +165,7 @@ export default {
      * - Set ให้ List Container แสดงเมื่อ currentValue มีการเปลี่ยนแปลง
      * - Set itemOnFocus = first item.
      */
-    currentValue (value) {
+    currentValue(value) {
       this.$emit('input', this.currentValue)
       if (this.hasFocus) {
         this.isOpenListContainer = true
@@ -138,48 +174,51 @@ export default {
     },
     /**
      * - ถ้า v-model เปลี่ยนค่าจากนอก Component
-     * - ให้เปลี่ยนค่า Internal value ด้วย 
+     * - ให้เปลี่ยนค่า Internal value ด้วย
      * - และซ่อน Dropdown
      */
-    value (value) {
+    value(value) {
       if (value !== this.currentValue) {
         this.currentValue = value
-        this.$nextTick(() => this.isOpenListContainer = false)
+        this.$nextTick(() => (this.isOpenListContainer = false))
       }
     },
     /**
      * - When color is changed: set internal value.
      */
-    color (val) {
+    color(val) {
       this.currentColor = val
     }
   },
   methods: {
-    onClickOutside (event) {
+    onClickOutside(event) {
       this.isOpenListContainer = false
     },
     /**
-    * Arrows keys listener.
-    */
-    pressArrow (direction) {
+     * Arrows keys listener.
+     */
+    pressArrow(direction) {
       if (direction === 'up') {
         this.setInputCursorToLastChar()
         this.itemOnFocus = this.itemOnFocus > 0 ? this.itemOnFocus - 1 : 0
       } else {
-        this.itemOnFocus = this.itemOnFocus < this.resultsFromSearch.length - 1 ? this.itemOnFocus + 1 : this.resultsFromSearch.length - 1
+        this.itemOnFocus =
+          this.itemOnFocus < this.resultsFromSearch.length - 1
+            ? this.itemOnFocus + 1
+            : this.resultsFromSearch.length - 1
       }
       this.moveScrollOfListContainer()
     },
     /**
-    * Enter button listener.
-    */
-    pressEnter () {
+     * Enter button listener.
+     */
+    pressEnter() {
       this.setSelectedValue(this.resultsFromSearch[this.itemOnFocus])
     },
     /**
-    * เลื่อน scroll bar ตาม Item ที่เลือก
-    */
-    moveScrollOfListContainer () {
+     * เลื่อน scroll bar ตาม Item ที่เลือก
+     */
+    moveScrollOfListContainer() {
       const list = this.$refs.dropdown
       const element = list.querySelectorAll('.list')[this.itemOnFocus]
       if (!element) return
@@ -188,33 +227,33 @@ export default {
       if (element.offsetTop < visMin) {
         list.scrollTop = element.offsetTop
       } else if (element.offsetTop >= visMax) {
-        list.scrollTop = (
-          element.offsetTop -
-          list.clientHeight +
-          element.clientHeight
-        )
+        list.scrollTop =
+          element.offsetTop - list.clientHeight + element.clientHeight
       }
     },
     /**
-    * Event on click item.
-    */
-    clickSelectItem (address) {
+     * Event on click item.
+     */
+    clickSelectItem(address) {
       this.setSelectedValue(address)
     },
-     /**
+    /**
      * - emit ค่าที่เลือกกลับไปยังฟังชั่นของ user
      * - Set currentValue = ข้อมูลที่ user เลือกไว้
      * - ซ่อน List Container
      */
-    setSelectedValue (address) {
+    setSelectedValue(address) {
       this.$emit('select', address)
-      this.type ? this.currentValue = address[this.type] : this._errorLog('type is undefined.')
-      this.$nextTick(() => this.isOpenListContainer = false)
+      this.type
+        ? (this.currentValue = address[this.type])
+        : this._errorLog('type is undefined.')
+      this.$nextTick(() => (this.isOpenListContainer = false))
     },
-     /**
+    /**
      * - หาค่า top ของ Dropwon Container
-     */ 
-    findListContainerPosition () {
+     */
+
+    findListContainerPosition() {
       let top = '36px'
       if (this.size === 'small') {
         top = '27px'
@@ -227,39 +266,61 @@ export default {
     },
     /**
      * - ทำให้ Cursor ของ input อยู่หลังสุดเสมอ
-     */ 
-    setInputCursorToLastChar () {
+     */
+
+    setInputCursorToLastChar() {
       const len = this.currentValue.length * 2
-      setTimeout(() => { this.$refs.input.setSelectionRange(len, len) }, 1)
+      setTimeout(() => {
+        this.$refs.input.setSelectionRange(len, len)
+      }, 1)
     },
     /**
      * - แสดงข้อมูลซ้ายบน
-     */ 
-    itemFirst (address) {
-      return this.type === 'district' ? address['amphoe'] : address['district']
+     */
+
+    itemFirst(address) {
+      let districtPrefix =
+        address['province'] == 'กรุงเทพมหานคร' ? 'แขวง : ' : 'ตำบล : '
+      return this.type === 'district'
+        ? '' + address['amphoe']
+        : districtPrefix + address['district']
     },
     /**
      * - แสดงข้อมูลตรงกลางบน
-     */ 
-    itemSecond (address) {
-      return this.type === 'amphoe' || this.type === 'district' ? address['province'] : address['amphoe']
+     */
+
+    itemSecond(address) {
+      let districtPrefix =
+        address['province'] == 'กรุงเทพมหานคร' ? 'เขต : ' : 'อำเภอ : '
+
+      console.log(districtPrefix)
+      return this.type === 'amphoe' || this.type === 'district'
+        ? '' + address['province']
+        : districtPrefix + address['amphoe']
     },
     /**
      * - แสดงข้อมูลขวาบน
-     */ 
-    itemThird (address) {
-      return this.type === 'province' || this.type === 'amphoe' || this.type === 'district' ? address['zipcode'] : address['province']
+     */
+
+    itemThird(address) {
+      return this.type === 'province' ||
+        this.type === 'amphoe' ||
+        this.type === 'district'
+        ? address['zipcode']
+        : address['province']
     },
     /**
      * - แสดงข้อมูลล่างซ้าย
-     */ 
-    itemFourth (address) {
+     */
+
+    itemFourth(address) {
       return address[this.type]
     },
     /**
      * - Error Log
-     */ 
-    _errorLog (text) {
+     */
+
+    _errorLog(text) {
       console.error(`[ERROR] vue-thailand-address-autocomplete : ${text}`)
     }
   }
@@ -277,7 +338,7 @@ export default {
 .container {
   float: left;
   width: 100%;
-  margin-bottom: .75rem;
+  margin-bottom: 0.75rem;
 }
 .input-container {
   float: left;
@@ -291,27 +352,27 @@ export default {
   letter-spacing: normal;
   text-align: left;
   color: #363636;
-  margin-bottom: .75rem;
+  margin-bottom: 0.75rem;
 }
 .input {
   float: left;
   width: 100%;
   border-radius: 3px;
   background-color: #ffffff;
-  padding: calc(.475em - 1px) .75rem;
+  padding: calc(0.475em - 1px) 0.75rem;
   line-height: 1.25;
   letter-spacing: normal;
   text-align: left;
   color: #363636;
   border: solid 1px #d3d3d3;
-  box-shadow: inset 0 1px 2px hsla(0,0%,4%,.1);
+  box-shadow: inset 0 1px 2px hsla(0, 0%, 4%, 0.1);
 }
-.input:focus{
+.input:focus {
   outline: none !important;
   border-radius: 3px;
 }
 .input-size-small {
-  font-size: .75rem;
+  font-size: 0.75rem;
 }
 .input-size-default {
   font-size: 1rem;
@@ -333,7 +394,7 @@ export default {
   background-color: #ffffff;
   border-top: solid 1px #f1f1f1;
   border-radius: 3px;
-  box-shadow: 0 2px 3px hsla(0,0%,4%,.1), 0 0 0 1px hsla(0,0%,4%,.1);
+  box-shadow: 0 2px 3px hsla(0, 0%, 4%, 0.1), 0 0 0 1px hsla(0, 0%, 4%, 0.1);
 }
 .list {
   float: left;
